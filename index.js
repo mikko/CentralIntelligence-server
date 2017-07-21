8a925dcd16ad4936e1920ec30c67ca0b365fb06e
Mikko Pohja <mikko.pohja@futurice.com>
2017-07-21 23:52:40 +0300

    Initial routing server implementation

---
+++ index.js
@@ -0,0 +1,70 @@
+const Joi = require('joi');
+const Hapi = require('hapi');
+const handlers = require('./lib/handlers');
+
+let serverOptions = {};
+
+const server = new Hapi.Server(serverOptions);
+
+const port = 3000;
+
+
+const routes = [
+    {
+        method: 'POST',
+        path: `/register`,
+        config: {
+            handler: handlers.register,
+            validate: {
+                payload: {
+                    name: Joi.string().required(),
+                    host: Joi.string().required(),
+                    port: Joi.number().required(),
+                    actions: Joi.object()
+                }
+            }
+        }
+    },
+    {
+        method: 'POST',
+        path: `/message`,
+        config: {
+            handler: handlers.message,
+            validate: {
+                payload: {
+                    message: Joi.string().required(),
+                    context: Joi.object().required()
+                }
+            }
+        }
+    },
+    {
+        method: 'POST',
+        path: `/command`,
+        config: {
+            handler: handlers.command,
+            validate: {
+                payload: {
+                    command: Joi.string().required(),
+                    context: Joi.object().required()
+                }
+            }
+        }
+    }
+];
+
+server.connection({
+    port
+});
+
+routes.forEach(route => {
+    server.route(route);
+});
+
+server.start(err => {
+    if (err) {
+        throw err;
+    }
+    console.log('Central Intelligence running', server.info.uri);
+
+});
